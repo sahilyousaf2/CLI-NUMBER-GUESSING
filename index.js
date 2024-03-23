0107ccce93fdb8c29e27c25c5ca822528cf34eaf
Sahil Yousaf <sahilyousaf66@gmail.com>
2024-03-23 12:47:41 +0500

    My Second Change

--- index.js
+++ index.js
@@ -3,17 +3,19 @@ import inquirer from "inquirer";
 //  1) Computer Will Generate A Random Number
 //  2) User  Input For Guesssing Number 
 //  3)  Compare User Input With Computer Generated Number And Show Result
-const randomNumber = Math.floor(Math.random() * 10) + 1; //Generates a random number between 1 and 10 (Inclusive).
-console.log("Welcom To Number Guessing Game");
+const randomNumber = Math.floor(Math.random() * 10 + 1); //Generates a random number between 1 and 10 (Inclusive).
+console.log("Welcome To Number Guessing Game");
 const answer = await inquirer.prompt([
-    { name: "userGuessedNumber",
+    {
+        name: "userGuessedNumber",
         type: "number",
         message: "Please Guess A Number"
     }
 ]);
 if (answer.userGuessedNumber == randomNumber) {
-    console.log("Congratulations You Are Winner");
+    console.log(`"Congratulations You Are The Winner"`);
 }
 else {
-    console.log("Your Lose");
+    console.log(`"You Lose"`);
+    console.log("Correct Number Is", randomNumber);
 }
